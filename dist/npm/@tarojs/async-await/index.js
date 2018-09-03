bd4e55b34bf005d8cf59836241a636cdb4e5e300
canisminor1990 <i@canisminor.cc>
2018-09-03 23:31:36 +0800

    update

--- dist/npm/@tarojs/async-await/index.js
+++
@@ -1,8 +0,0 @@
-var global = typeof window !== 'undefined' && window.Math === Math ? window : typeof self !== 'undefined' && self.Math === Math ? self : this;
-
-if (!global.Promise) {
-  global.Promise = require("../../promise-polyfill/lib/index.js");
-}
-if (!global.regeneratorRuntime) {
-  global.regeneratorRuntime = require("../../regenerator-runtime/runtime.js");
-}
