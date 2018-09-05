451a3c275c2fd5d75e2f997ff0904dd96efdc6fa
canisminor1990 <i@canisminor.cc>
2018-09-05 11:14:20 +0800

    v2.0.1

--- config/index.js
+++ config/index.js
@@ -18,7 +18,8 @@ const config = {
 			plugins  : [
 				'transform-class-properties',
 				'transform-decorators-legacy',
-				'transform-object-rest-spread'
+				'transform-object-rest-spread',
+				'lodash'
 			]
 		},
 		typescript: {
